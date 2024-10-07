b9058fd33505f6e936da17082be248d983a4f146
OneKekBer <iliaberezovsky227@gmail.com>
2024-10-07 16:33:15 +0200

    save

--- SeaBattleWeb.Data/Entities/Board.cs
+++ SeaBattleWeb.Data/Entities/Board.cs
@@ -1,23 +1,25 @@
 ﻿using SeaBattleWeb.Data.GameLogic.Models.Board;
 using SeaBattleWeb.Data.GameLogic.Models.Values;
-
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
 
 namespace SeaBattleWeb.Data.Entities
-{   
+{
     public class Board
     {
         public Board()
         {
-            
+
         }
 
-        public Board(Guid userId, Guid gameId)
+        public Board(Guid userId, Guid gameId) : this()
         {
             UserId = userId;
             GameId = gameId;
         }
 
-        public Panel[,] board = new Panel[9, 9];
+        [NotMapped]
+        public Panel[,] board { get; set; }
 
         public Panel this[Coordinates coords]
         {
@@ -25,10 +27,43 @@ namespace SeaBattleWeb.Data.Entities
             set => board[coords.Y, coords.X] = value;
         }
 
+        public void SerializeArray()
+        {
+            var boardJaggedArray = new Panel[9][];
+            for (int i = 0; i < 9; i++)
+            {
+                boardJaggedArray[i] = new Panel[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    boardJaggedArray[i][j] = board[i, j];
+                }
+            }
+
+            BoardSerialized = JsonSerializer.Serialize(boardJaggedArray);
+        }
+
+        public void DeserializeArray()
+        {
+            if (!string.IsNullOrEmpty(BoardSerialized))
+            {
+                var boardJaggedArray = JsonSerializer.Deserialize<Panel[][]>(BoardSerialized);
+
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        board[i, j] = boardJaggedArray[i][j];
+                    }
+                }
+            }
+        }
+
+        public string BoardSerialized { get; set; }
+
         public Guid Id { get; init; } = Guid.NewGuid();
 
         public Guid UserId { get; init; }
 
-        public Guid GameId {  get; init; }
+        public Guid GameId { get; init; }
     }
 }
---
+++ SeaBattleWeb.UnitTests/BoardUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using SeaBattleWeb.Data.Entities;
+using SeaBattleWeb.Data.GameLogic.Models.Board;
+using SeaBattleWeb.Data.GameLogic.Models.Values;
+using SeaBattleWeb.GameLogic.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace SeaBattleWeb.UnitTests
+{
+    public class BoardUnitTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public BoardUnitTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void SerializeAndDeseialize_WhenDeserializeAndSerializeBoard_BoardIsWorkingCorrectly()
+        {
+            //Arrange
+            var board = new Board();
+            var shipPlacer = new ShipPlacer();
+
+            //Act
+            shipPlacer.FillEmptyBoard(board);
+
+            board.SerializeArray();
+
+            board.DeserializeArray();
+            // Check if the entire board is filled with empty panels
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Assert.NotNull(board[new Coordinates(i, j)]);
+                    Assert.Equal(PanelState.Empty, board[new Coordinates(i, j)].PanelState);
+                }
+            }
+        }
+    }
+}
